8c91fc6ed71f23e82916503d0c53529c4d0cef1d
nastassjakratochvil <gv>
2021-03-19 15:45:56 +0100

    Correction bug du fichier WriteResultFromMap : problème de placement des chevrons

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteResultFromMap.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteResultFromMap.java
@@ -8,14 +8,13 @@ import java.util.TreeMap;
 public class WriteResultFromMap {
 
     void WriteResultFromMap(SortedMap <String, Integer> map) {
-        try{
+        try {
         FileWriter writer = new FileWriter("result.out");
         for (Entry<String, Integer> entry : map.entrySet()) {
-            map.entrySet();
             writer.write(entry.getKey() + " = " + entry.getValue() + "\n");
+        }
             writer.close();
             System.out.println("The following doc was successfully processed");
-        }
 
     } catch (Exception e) {
             System.out.println("The file was not found");
